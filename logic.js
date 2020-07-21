cde810006b64390e84babcff07965e953df07ff7
Subritt <subrittburlakoti@gmail.com>
2020-07-21 23:28:37 +0545

    function to add list and test inputs

--- logic.js
+++ logic.js
@@ -1,7 +1,31 @@
 const inputElement = document.querySelector('#input');
 const convertBtn = document.querySelector('#convert-btn');
 
-convertBtn.addEventListener('click', () => {
+convertBtn.addEventListener('click', e => {
     console.log(inputElement.value);
+
+    let usdToNpr = result(input.value);
+    addDOM(usdToNpr);
+
     inputElement.value = '';
 });
+
+// Convert currency
+let result = amount => {
+    return (amount * 119.39);
+}
+
+// Add DOM
+function addDOM(usdToNpr) {
+    let ui = document.createElement('ui');
+    ui.classList = 'list-group';
+    
+    let li = document.createElement('li');
+    li.classList = 'list-group-item';
+    li.textContent = `Rs.${usdToNpr}`;
+
+    ui.appendChild(li);
+    document.querySelector('#result-div').appendChild(ui);
+
+    console.log(ui);
+}
